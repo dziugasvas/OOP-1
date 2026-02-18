b415963b5448ba4b3dceada815fcd8af5a31f3df
dziugasvas <dziugasvas@gmail.com>
2026-02-18 20:06:24 +0200

    Paskaitos versija

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::cin;
+using std::vector;
+using std::cout;
+using std::left;
+using std::right;
+using std::setw;
+using std::endl;
+
+struct Studentas {
+    string vardas, pavarde;
+    vector <int> paz;
+    int egz;
+    double rez;
+};
+
+void inputas (vector <Studentas> &grupe);
+void outputas (const vector <Studentas> &grupe);
+int main() {
+    vector <Studentas> grupe;
+    inputas(grupe);
+    outputas(grupe);
+}
+
+void inputas (vector <Studentas> &grupe){
+    for(int ii = 0; ii < 2; ii++) {
+    Studentas A;
+    cout << "Iveskite studento vardo ir pavarde: ";
+    cin >> A.vardas >> A.pavarde;
+    cout << "Iveskite semestro ivercius. Kiek ju bus? ";
+    int n, sum = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int temp;
+        cout << "Iveskite " << i + 1 << "-aji pazymi is " << n << ": ";
+        cin >> temp;
+        A.paz.push_back(temp); sum += temp;
+        }
+    cout << "Iveskite egzamina: "; cin >> A.egz;
+    A.rez = sum * 1.0 / (n * 1.0) * 0.4 + A.egz * 0.6;
+    grupe.push_back(A);
+    A.paz.clear();
+    }
+}
+void outputas(const vector <Studentas> &grupe){
+    for(auto A: grupe){
+        cout << left << setw(10) << A.vardas << left << setw(20) << A.pavarde;
+        //for(auto k: A.paz) cout<<setw(3)<<k;
+        //cout<<setw(5)<<A.egz;
+        cout << setw(10) << A.rez;
+    }
+}
+
