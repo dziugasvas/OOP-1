f18f4dfdcaec5db0685b61ce0393cb1e8e28a41d
dziugasvas <dziugasvas@gmail.com>
2026-02-19 21:24:06 +0200

    Pridėti pradiniai masyvų ir vektorių failai

---
+++ Masyvai.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Masyvu pradine versija" << endl;
+
+    return 0;
+}
---
+++ Vektoriai.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Vektoriu pradine versija" << endl;
+
+    return 0;
+}
