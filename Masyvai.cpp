444d26770a2b925b168226ec4987443ed1062291
dziugasvas <dziugasvas@gmail.com>
2026-02-20 00:32:01 +0200

    Pridetas Masyvai.cpp pradinis failas su struktura

--- Masyvai.cpp
+++ Masyvai.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
 
-using namespace std;
+using std::string;
+using std::cin;
+using std::cout;
+using std::left;
+using std::right;
+using std::setw;
+using std::endl;
+
+struct Studentas {
+    string vardas, pavarde;
+    int* nd = nullptr;
+    int kiekis = 0;
+    int egzaminas = 0;
+};
 
 int main() {
-
-    cout << "Masyvu pradine versija" << endl;
+    Studentas* A = nullptr;
+    int m = 0;
 
     return 0;
 }
