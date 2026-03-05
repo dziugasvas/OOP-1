fb3ab04d41ec0daf6c42a511ed877a81068d84d1
dziugasvas <dziugasvas@gmail.com>
2026-03-05 19:45:59 +0200

    Pridetas try/catch main.cpp

--- programa/main.cpp
+++ programa/main.cpp
@@ -141,11 +141,14 @@ int main() {
                 cout << "Iveskite failo pavadinima: ";
                 cin >> failas;
 
+                try {
                 auto pradzia = std::chrono::high_resolution_clock::now();
                 nuskaitymas(grupe, failas);
                 auto laikas = std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - pradzia).count();
-
                 cout << "Nuskaitymo laikas: " << std::fixed << std::setprecision(3) << laikas << " s" << endl;
+                } catch (std::exception& e) {
+                    cout << "Klaida: " << e.what() << endl;
+                }
                 break;
             }
 
@@ -174,10 +177,14 @@ int main() {
                     cin >> kur;
                 }
 
+                try {
                 if (kur == 'f') {
                     spausdinimas(grupe, budas);
                 } else {
                     outputas(grupe, budas);
+                }
+                } catch (std::exception& e) {
+                    cout << "Klaida: " << e.what() << endl;
                 }
 
                 veikia = false;
@@ -192,3 +199,4 @@ int main() {
 
     return 0;
 }
+
