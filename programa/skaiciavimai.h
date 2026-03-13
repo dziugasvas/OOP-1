3c9a5b778a354b39c0248bdab7d60fd1d7241896
dziugasvas <dziugasvas@gmail.com>
2026-03-13 19:50:01 +0200

    Studentų skirstymas į vargsiukai ir kietakai

--- programa/skaiciavimai.h
+++ programa/skaiciavimai.h
@@ -7,6 +7,7 @@
 double mediana(const std::vector<int>& paz);
 double vidurkis(const std::vector<int>& paz);
 void rusiavimas(std::vector<Studentas>& grupe, char budas);
+void padalintiStudentus(const std::vector<Studentas>& grupe, std::vector<Studentas>& vargsiukai, std::vector<Studentas>& kietakai, char budas);
 
 #endif
 
